bf36de68f3bbc6f06e2c97c9c296d10d55bb462f
Nianyi Wang <Wangnianyi2001@outlook.com>
2023-06-22 03:12:37 +0800

    Support using enum values directly when tagging ShowIfEnum attribute

--- Assets/Nianyi/Modules/Attributes/Editor/ShowIfEnumAttributeDrawer.cs
+++ Assets/Nianyi/Modules/Attributes/Editor/ShowIfEnumAttributeDrawer.cs
@@ -1,17 +1,32 @@
 using UnityEditor;
 using UnityEngine;
+using System.Linq;
 
 namespace Nianyi.Editor {
 	[CustomPropertyDrawer(typeof(ShowIfEnumAttribute))]
 	public class ShowIfEnumAttributeDrawer : PropertyDrawerBase {
 		bool show = false;
 
+		protected static bool DetermineVisibility(ShowIfEnumAttribute attribute, SerializedProperty member) {
+			if(attribute == null)
+				return false;
+			if(attribute.expectedEnumValues.Length <= 0)
+				return true;
+
+			SerializedProperty showProperty = member.serializedObject.FindProperty(attribute.propertyName);
+			if(showProperty == null)
+				return false;
+
+			var enumType = attribute.expectedEnumValues[0].GetType();
+			var enumValues = enumType.GetEnumValues().Cast<System.Enum>();
+			var actualIndex = showProperty.enumValueIndex;
+
+			return attribute.expectedEnumValues.Any(expected => System.Convert.ToInt32(expected) == actualIndex);
+		}
+
 		protected override void Draw(SerializedProperty member, GUIContent label) {
-			if(!Rendering) {
-				var showIf = attribute as ShowIfEnumAttribute;
-				SerializedProperty showProperty = member.serializedObject.FindProperty(showIf.propertyName);
-				show = showProperty != null ? showProperty.enumNames[showProperty.enumValueIndex] == showIf.expected : false;
-			}
+			if(!Rendering)
+				show = DetermineVisibility(base.attribute as ShowIfEnumAttribute, member);
 			if(show)
 				PropertyField(member, label);
 		}
--- Assets/Nianyi/Modules/Attributes/ShowIfEnumAttribute.cs
+++ Assets/Nianyi/Modules/Attributes/ShowIfEnumAttribute.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 namespace Nianyi {
 	[System.AttributeUsage(System.AttributeTargets.Field, AllowMultiple = true)]
 	public class ShowIfEnumAttribute : PropertyAttribute {
 		public readonly string propertyName;
-		public readonly string expected;
+		public readonly System.Enum[] expectedEnumValues;
 
-		public ShowIfEnumAttribute(string propertyName, string expected) {
+		public ShowIfEnumAttribute(string propertyName, params object[] expectedEnumValues) {
 			this.propertyName = propertyName;
-			this.expected = expected;
+			this.expectedEnumValues = expectedEnumValues
+				.Cast<System.Enum>()
+				.Where(value => value != null)
+				.ToArray();
 		}
 	}
 }
