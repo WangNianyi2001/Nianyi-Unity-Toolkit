b49c0471c057779fe9217a0fea89cf84cf4f5ce3
Nianyi Wang <wangnianyi2001@outlook.com>
2024-12-30 03:32:45 +0800

    Trim unnecessary files in the dynamic mesh package

--- dynamic-mesh/Runtime/DynamicMesh.cs
+++ dynamic-mesh/Runtime/DynamicMesh.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections.Generic;
 
 namespace Nianyi.UnityToolkit
 {
-	public partial class DynamicMesh : ScriptableObject
+	public partial class DynamicMesh
 	{
-		#region Internal fields
-		private Mesh importedMesh;
 		private struct MinMaxRange<T>
 		{
 			public T min, max;
@@ -21,34 +18,9 @@ namespace Nianyi.UnityToolkit
 			public MinMaxRange(MinMaxRange<T> range) : this(range.min, range.max) { }
 		}
 		private MinMaxRange<Vector3> range;
-		#endregion
 
-		#region Serialized fields
-		public Mesh sourceMesh;
-		[HideInInspector] public readonly List<UnityDcel> submeshData = new();
+		public readonly List<UnityDcel> submeshes = new();
 
-		[Serializable]
-		public struct ImportOptions
-		{
-			[Tooltip("Auto re-import mesh data when source mesh changes")]
-			public bool autoReimport;   // TODO: auto detect mesh change
-
-			public bool limitVertexCount;
-			[ShowWhen("limitVertexCount", true)]
-			[Min(0)] public int maxVertexCount;
-
-			public bool useGridOptimization;
-			[ShowWhen("useGridOptimization", true)]
-			public bool controlGridSize;
-			[ShowWhen("useGridOptimization", true)]
-			[ShowWhen("controlGridSize", true)]
-			[Tooltip("Desired vertex count per grid")]
-			[Min(2)] public int desiredGridSize;
-		}
-		public ImportOptions importOptions;
-		#endregion
-
-		#region Public interfaces
 		public Vector3 Size => new(
 			Mathf.Abs(range.max[0] - range.min[0]),
 			Mathf.Abs(range.max[1] - range.min[1]),
@@ -60,7 +32,7 @@ namespace Nianyi.UnityToolkit
 			get
 			{
 				int count = 0;
-				foreach(var submesh in submeshData)
+				foreach(var submesh in submeshes)
 					count += submesh.VertexCount;
 				return count;
 			}
@@ -69,7 +41,7 @@ namespace Nianyi.UnityToolkit
 		{
 			get
 			{
-				foreach(var submesh in submeshData)
+				foreach(var submesh in submeshes)
 				{
 					foreach(var vertex in submesh.vertices)
 						yield return vertex;
@@ -82,45 +54,22 @@ namespace Nianyi.UnityToolkit
 			get
 			{
 				int count = 0;
-				foreach(var submesh in submeshData)
+				foreach(var submesh in submeshes)
 					count += submesh.SurfaceCount;
 				return count;
 			}
 		}
 
-		public Mesh ImportedMesh
+		public Mesh ToMesh(string name)
 		{
-			get
-			{
-				if(submeshData == null)
-					return importedMesh = null;
-				if(importedMesh == null)
-				{
-					importedMesh = new Mesh();
-					for(int i = 0; i < submeshData.Count; ++i)
-						submeshData[i].WriteToMesh(importedMesh, i);
-				}
-				return importedMesh;
-			}
-		}
+			if(submeshes == null)
+				return null;
 
-		public void Reset()
-		{
-			submeshData.Clear();
-			importedMesh = null;
-			range = new MinMaxRange<Vector3>(Vector3.one * Mathf.Infinity, -Vector3.one * Mathf.Infinity);
-		}
+			Mesh mesh = new() { name = name };
+			for(int i = 0; i < submeshes.Count; ++i)
+				submeshes[i].WriteToMesh(mesh, i);
 
-		public void ReimportMeshData()
-		{
-			Reset();
-			if(sourceMesh == null || !sourceMesh.isReadable)
-				return;
-
-			for(int i = 0; i < sourceMesh.subMeshCount; ++i)
-				submeshData.Add(ConstructSubmeshDataFromMesh(sourceMesh, i));
-			range = CalculateVertexRange(submeshData);
+			return mesh;
 		}
-		#endregion
 	}
 }
