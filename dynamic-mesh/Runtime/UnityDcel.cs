8b366f6ba672838b75e7922901959b0dc45900ef
Nianyi Wang <wangnianyi2001@outlook.com>
2024-12-30 19:03:49 +0800

    Add useful functionalities for dynamic mesh

--- dynamic-mesh/Runtime/UnityDcel.cs
+++ dynamic-mesh/Runtime/UnityDcel.cs
@@ -14,6 +14,16 @@ namespace Nianyi.UnityToolkit
 		public new class Vertex : Dcel.Vertex
 		{
 			public Vector3 position;
+			public Vector3 normal = Vector3.forward;
+			public Vector2 uv;
+
+			public Vertex() { }
+			public Vertex(Vertex v)
+			{
+				position = v.position;
+				normal = v.normal;
+				uv = v.uv;
+			}
 		}
 
 		public new class Surface : Dcel.Surface
@@ -21,31 +31,64 @@ namespace Nianyi.UnityToolkit
 		}
 
 		#region Public interfaces
-		public void WriteToMesh(Mesh mesh, int submeshIndex, int vertexIndexOffset = -1)
+		public void WriteToMesh(in Mesh mesh, int submeshIndex)
 		{
-			if(vertexIndexOffset < 0)
-				vertexIndexOffset = mesh.vertexCount;
+			int originalVertexCount = mesh.vertexCount;
+			int newCount = originalVertexCount + vertices.Count;
 
-			List<Vector3> vertexList = new();
-			mesh.GetVertices(vertexList);
-			List<Vector3> newVertexList = vertices.Select(v => v.position).ToList();
+			List<Vector3> positions = new(newCount);
+			mesh.GetVertices(positions);
+			List<Vector3> normals = new(newCount);
+			mesh.GetNormals(normals);
+			List<Vector2> uvs = new(newCount);
+			mesh.GetUVs(0, uvs);
 
-			int i;
-			// Replace vertices until reach the end of the original vertex list.
-			for(i = 0; i + vertexIndexOffset < vertexList.Count() && i < vertexList.Count(); ++i)
-				vertexList[i + vertexIndexOffset] = newVertexList[i];
-			newVertexList.RemoveRange(0, i);
-			// Add rest vertices.
-			vertexList.AddRange(newVertexList);
+			for(int i = 0; i < vertices.Count; ++i)
+			{
+				positions.Add(vertices[i].position);
+				normals.Add(vertices[i].normal);
+				uvs.Add(vertices[i].uv);
+			}
 
-			mesh.SetVertices(vertexList);
+			mesh.SetVertices(positions);
+			mesh.SetNormals(normals);
+			mesh.SetUVs(0, uvs);
+
+			// Triangles.
 			var triangles = new List<int>();
 			foreach(var surface in surfaces)
 			{
 				foreach(var vertex in surface.Vertices)
-					triangles.Add(vertices.IndexOf(vertex) + vertexIndexOffset);
+					triangles.Add(vertices.IndexOf(vertex) + originalVertexCount);
 			}
 			mesh.SetTriangles(triangles, submeshIndex);
+		}
+
+		public void CalculateNormals()
+		{
+			foreach(Vertex v in vertices)
+			{
+				foreach(HalfEdge he in v.outGoingHalfEdges)
+				{
+					Vertex a = he.To, b = he.next.To;
+					v.normal = Vector3.Cross(v.position - a.position, v.position - b.position).normalized;
+				}
+			}
+		}
+
+		public void SplitSurfaces()
+		{
+			List<Surface> oldSurfaces = surfaces, newSurfaces = new();
+			surfaces = newSurfaces;
+			halfEdges = new List<HalfEdge>();
+			vertices = new List<Vertex>();
+
+			foreach(var surface in oldSurfaces)
+			{
+				var copy = surface.Vertices.Select(v => new Vertex(v)).ToArray();
+				vertices.AddRange(copy);
+				CreateSurface(copy[0], copy[1], copy[2]);
+			}
 		}
 		#endregion
 	}
